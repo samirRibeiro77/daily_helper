d03e856b779afe7b2323b6f71a6a048a514e8f8a
samirribeiro <samir.sjr77@gmail.com>
2020-03-07 11:54:12 -0300

    Init app

---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:daily_helper/ui/home_screen.dart';
+import 'package:daily_helper/util/string_key.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'app_localizations.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: AppLocalizations.of(context).translate(StringKey.APP_NAME),
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+        brightness: Brightness.light
+      ),
+      darkTheme: ThemeData(
+        primarySwatch: Colors.red,
+        brightness: Brightness.dark
+      ),
+      supportedLocales: [
+        Locale('en', 'US'),
+        Locale('pt', 'BR')
+      ],
+      localizationsDelegates: [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      localeResolutionCallback: (locale, supportedLocales) {
+        for (var supportedLocale in supportedLocales) {
+          if (supportedLocale.languageCode == locale.languageCode
+              && supportedLocale.countryCode == locale.countryCode) {
+            return supportedLocale;
+          }
+        }
+        return supportedLocales.first;
+      },
+      home: HomeScreen(),
+    );
+  }
+}
---
+++ lib/util/string_key.dart
@@ -0,0 +1,3 @@
+class StringKey {
+  static const String APP_NAME = 'app_name';
+}
