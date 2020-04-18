40688adfc40533dcd1f8ac346ab95c3254aa9483
Samir Ribeiro <samir.sjr77@gmail.com>
2020-04-18 20:43:10 -0300

    App.
    New Icon.

--- lib/main.dart
+++ lib/main.dart
@@ -43,7 +43,6 @@ class MyApp extends StatelessWidget {
               }
             }
             catch(e) {
-              print(e.toString());
               return Locale('en', 'US');
             }
           }
